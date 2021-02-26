e20c7bb1965539fa2b14b3833e79b004bb936080
YabMas <jaapmaaskant@hotmail.com>
2021-02-26 11:39:34 +0100

    endpoint ophalen enkel item toegevoegd

--- src/main/java/nl/han/dea/ItemResource.java
+++ src/main/java/nl/han/dea/ItemResource.java
@@ -1,15 +1,14 @@
 package nl.han.dea;
 
 import nl.han.dea.services.ItemService;
-import nl.han.dea.services.dto.ItemDTO;
 
-import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
 
 @Path("/items")
 public class ItemResource {
